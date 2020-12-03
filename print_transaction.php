da46f7389c6dfbe384fe7260079194cd01f40210
roniespeedster <ronieB03@gmail.com>
2020-12-03 09:08:53 +0800

    Update print_transaction.php

--- print_transaction.php
+++ print_transaction.php
@@ -37,13 +37,13 @@ $balance = $newTransaction['amount_paid'] - $newTransaction['total_amount'];
             ?>
         <!-- Begin Page Content -->
         <p style="page-break-before: always">
-        <div class="container-fluid">
+        <div class="container-fluid print_area" style="color: black; border-bottom: solid grey 1px;" >
             <br>
             <br>
             <!-- Page Heading -->
             <div class="align-items-center justify-content-between mb-4" >
-                <h4 style="text-align: center !important; font-family: 'Times New Roman' !important; margin-bottom: -5px;">
-                    <img src="img/logo.png" style="width: 40px;">
+                <h4 style="text-align: center !important; font-family: 'Times New Roman' !important; position: relative; z-index: 99; line-height: 0;">
+                    <img src="img/logo.png" style="width: 50px; position: absolute; z-index: -1; top: -20px;  left: 20px;">
                     SYSTEMS PLUS COLLEGE FOUNDATION
                 </h4>
                 <div style="text-align: center;">Mc Arthur Hi-Way Balibago, Angeles City, Pampanga</div>
@@ -52,20 +52,21 @@ $balance = $newTransaction['amount_paid'] - $newTransaction['total_amount'];
             <!-- View Individual Transactions -->
             <div class="mb-4">
                 <div class="card-header py-3">
-                    <span class="h6 m-0 font-weight-bold text-danger">Transaction Control ID: <?php echo sprintf('%08d',$id); ?></span>
-                    <span class=" h6 m-0 font-weight-bold text-danger float-right">Series No: <?php echo sprintf('%08d',$series_id); ?></span>
+                    <span class="h6 m-0 font-weight-bold text-danger" style="display: none;">Transaction Control ID: <?php echo sprintf('%08d',$id); ?></span>
+                    <span class=" h6 m-0 font-weight-bold text-danger text-uppercase">Acknowledgement Receipt: <?php echo sprintf('%08d',$series_id); ?></span>
                 </div>
                 <div class="card-body">
-                    <span class="float-right">Date: <b><?php echo date('Y-m-d H:i:s');?></b></span>
-                    Customer Name: <b><?php echo $newTransaction['full_name']; ?></b>
+                    <span class="float-right">
+                        <b>Date: <?php echo date('Y-m-d H:i:s');?></b></span>
+                    <b>Customer Name: <?php echo ucwords($newTransaction['full_name']); ?></b>
                     <br>
-                    Address: <b><?php echo $newTransaction['address']; ?></b>
+                    <b>Address: <?php echo ucwords($newTransaction['address']); ?></b>
                     <br>
-                    Phone Number: <b><?php echo $newTransaction['phone_num']; ?></b>
+                    <b>Phone Number: <?php echo $newTransaction['phone_num']; ?></b>
                     <br>
                     <br>
                     <div class="table-responsive">
-                        <table class="table table-bordered">
+                        <table class="table table-bordered" style="color: black;">
                             <thead>
                             <tr>
                                 <th>Product</th>
@@ -102,15 +103,13 @@ $balance = $newTransaction['amount_paid'] - $newTransaction['total_amount'];
                             </tbody>
                         </table>
 
-                        <span class="float-right"><h6><b>Total Amount Paid: ₱<?php echo number_format($newTransaction['amount_paid'],2); ?></b></h6></span>
+                        <b class="float-right">Total Amount Paid: ₱<?php echo number_format($newTransaction['amount_paid'],2); ?></b>
                         <br>
-                        <br>
-                        <span class="float-right"><h6><b>Change: ₱<?php echo number_format($newTransaction['amount_paid']-$newTransaction['total_amount'],2); ?></b></h6></span>
-                        <br>
-                        <br>
-                        <span class="float-right"><h6><b style="display: none; color: <?php if($balance<0){ echo 'red';}else{echo 'green';} ?>">Balance: <?php echo $balance; ?></b></h6></span>
+                        <b class="float-right">Change: ₱<?php echo number_format($newTransaction['amount_paid']-$newTransaction['total_amount'],2); ?></b>
+                        <br/>
+                        <br/>
                         <div style="">
-                            <span class="float-right">Cashier & Signature: <u>_____<?php echo ucfirst($_SESSION['account_full_name']); ?>_____</u></span>
+                            <span class="float-right"><b>Cashier: <u><?php echo ucfirst($_SESSION['account_full_name']); ?></b></u></span>
                             <a style="display: none;" href="view_transaction.php?id=<?php echo $id; ?>" class="text-white btn btn-sm btn-info float-left"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</a>
                             <?php if($counter==1){ ?>
                                 STUDENT'S COPY
@@ -149,6 +148,11 @@ $balance = $newTransaction['amount_paid'] - $newTransaction['total_amount'];
             display: none;
         }
         html{
-            font-size: 12px;
+            font-size: 14px;
+        }
+        @media print {
+            .print_area {
+                min-height: 5.5in;
+            }
         }
     </style>
